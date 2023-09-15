bb4366d6a69464f9b648d57253e8640551dcc547
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-15 12:33:25 +0530

    added algorithms.json

--- AlgoTeacherWPF/Model/Algorithm.cs
+++ AlgoTeacherWPF/Model/Algorithm.cs
@@ -1,11 +1,15 @@
-﻿namespace AlgoTeacherWPF.Model;
+﻿using AlgoTeacherWPF.Model.Enums;
+
+namespace AlgoTeacherWPF.Model;
 
 public class Algorithm
 {
     public int Id { get; set; }
+    public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
-    public TimeComplexity WorstCaseComplexity { get; set; } = null!;
-    public TimeComplexity AverageCaseComplexity { get; set; } = null!;
-    public TimeComplexity BestCaseComplexity { get; set; } = null!;
+    public Complexity WorstCaseComplexity { get; set; } = null!;
+    public Complexity AverageCaseComplexity { get; set; } = null!;
+    public Complexity BestCaseComplexity { get; set; } = null!;
+    public BigOComplexity SpaceComplexity { get; set; }
     public bool IsStable { get; set; }
 }
--- AlgoTeacherWPF/Model/TimeComplexity.cs
+++ AlgoTeacherWPF/Model/TimeComplexity.cs
@@ -2,7 +2,7 @@
 
 namespace AlgoTeacherWPF.Model;
 
-public class TimeComplexity
+public class Complexity
 {
     public Case Case { get; set; }
     public BigOComplexity BigOComplexity { get; set; }
