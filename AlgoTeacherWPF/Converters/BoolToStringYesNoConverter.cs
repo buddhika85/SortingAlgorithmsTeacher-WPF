830863b03e3a8aa65960812a27190f28b680fa7a
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-25 12:06:42 +0530

    bool to string converter added

---
+++ AlgoTeacherWPF/Converters/BoolToStringYesNoConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AlgoTeacherWPF.Converters
+{
+    public class BoolToStringYesNoConverter : IValueConverter
+    {
+        private const string Yes = "Yes";
+        private const string No = "No";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Yes;
+            return No;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)value).Equals(Yes);
+        }
+    }
+}
--- AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
+++ AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
@@ -6,6 +6,26 @@ namespace AlgoTeacherWPF.ViewModel
     {
         public Algorithm Algorithm { get; init; }
 
+        // This constructor is used for testing and UI design purposes
+        public AlgorithmDetailViewModel()
+        {
+            Algorithm = new Algorithm
+            {
+                Id = 1,
+                Name = "X sort",
+                Description =
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s," +
+                    " when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                WorstCaseComplexity = "O(n^2)",
+                BestCaseComplexity = "O(n)",
+                AverageCaseComplexity = "O(n^2)",
+                SpaceComplexity = "O(1)",
+                IsStable = false,
+                Presentation = new Presentation { BackgroundColor = "Red" }
+            };
+        }
+
+        // This constructor will be invoked when MainView algorithm selection button gets clicked
         public AlgorithmDetailViewModel(Algorithm algorithm)
         {
             Algorithm = algorithm;
