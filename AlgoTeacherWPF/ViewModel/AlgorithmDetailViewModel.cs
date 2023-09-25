15d06546096b8e6c53dce70f95ee219cecdc5871
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-25 14:05:53 +0530

    unsroted data set array view added

--- AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
+++ AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using AlgoTeacherWPF.Model;
+using System.Collections.Generic;
 
 namespace AlgoTeacherWPF.ViewModel
 {
     public class AlgorithmDetailViewModel : BaseViewModel
     {
         public Algorithm Algorithm { get; init; }
+
+        private List<int> _unsortedDataSet;
+
+        public List<int> UnsortedDataSet
+        {
+            get => _unsortedDataSet;
+            set
+            {
+                _unsortedDataSet = value;
+                OnPropertyChanged(nameof(UnsortedDataSet));
+            }
+        }
+
+
+
+        #region constructors
 
         // This constructor is used for testing and UI design purposes
         public AlgorithmDetailViewModel()
@@ -23,12 +40,16 @@ namespace AlgoTeacherWPF.ViewModel
                 IsStable = false,
                 Presentation = new Presentation { BackgroundColor = "Red" }
             };
+            _unsortedDataSet = new List<int> { 50, 40, 30, 20, 10 };
         }
 
         // This constructor will be invoked when MainView algorithm selection button gets clicked
         public AlgorithmDetailViewModel(Algorithm algorithm)
         {
             Algorithm = algorithm;
+            _unsortedDataSet = new List<int> { 50, 40, 30, 20, 10 };
         }
+
+        #endregion constructors
     }
 }
