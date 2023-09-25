79701eac317a5b9dce9dec993dccfeca19011cb6
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-25 19:13:20 +0530

    Refactoring - moved code to util classes

--- AlgoTeacherWPF/Utilities/Util.cs
+++ AlgoTeacherWPF/Utilities/Util.cs
@@ -1,18 +1,66 @@
-﻿using System;
+﻿using AlgoTeacherWPF.Model.Enums;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace AlgoTeacherWPF.Utilities
 {
     public static class Util
     {
-        public static void GenerateRandomDataSet(int min, int max, int datSetSize, ref ObservableCollection<int> unsortedDataSet)
+        public static void GenerateRandomDataSet(int min, int max, int datSetSize, ComplexityCase complexityCase, ref ObservableCollection<int> unsortedDataSet)
+        {
+            var tempList = GetRandomIntList(min, max, datSetSize);
+            tempList.Sort();
+            ArrangeIntListByComplexityCase(complexityCase, tempList);
+            PopulateObservableCollection(unsortedDataSet, tempList);
+        }
+
+        public static void ReArrangeDataSet(ComplexityCase complexityCase, ref ObservableCollection<int> unsortedDataSet)
+        {
+            var tempList = unsortedDataSet.ToList();
+            tempList.Sort();
+            ArrangeIntListByComplexityCase(complexityCase, tempList);
+            PopulateObservableCollection(unsortedDataSet, tempList);
+        }
+
+        private static void PopulateObservableCollection(ObservableCollection<int> unsortedDataSet, List<int> tempList)
+        {
+            unsortedDataSet.Clear();
+            foreach (var item in tempList)
+            {
+                unsortedDataSet.Add(item);
+            }
+        }
+
+        private static void ArrangeIntListByComplexityCase(ComplexityCase complexityCase, List<int> tempList)
+        {
+            switch (complexityCase)
+            {
+                case ComplexityCase.BestCase:
+                    // already sorted
+                    break;
+                case ComplexityCase.WorstCase:
+                    tempList.Reverse();
+                    break;
+                case ComplexityCase.AverageCase:
+                default:
+                    // mix first and last
+                    (tempList[0], tempList[^1]) = (tempList[^1], tempList[0]);
+                    break;
+            }
+        }
+
+        private static List<int> GetRandomIntList(int min, int max, int datSetSize)
         {
             Random rnd = new();
-            unsortedDataSet.Clear();
+            List<int> tempList = new();
             for (var i = 0; i < datSetSize; i++)
             {
-                unsortedDataSet.Add(rnd.Next(min, max + 1));
+                tempList.Add(rnd.Next(min, max + 1));
             }
+
+            return tempList;
         }
     }
 }
--- AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
+++ AlgoTeacherWPF/ViewModel/AlgorithmDetailViewModel.cs
@@ -3,15 +3,15 @@ using AlgoTeacherWPF.Model.Enums;
 using AlgoTeacherWPF.Utilities;
 using AlgoTeacherWPF.ViewModel.Commands;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace AlgoTeacherWPF.ViewModel
 {
     public class AlgorithmDetailViewModel : BaseViewModel
     {
         private const int InitialDatSetSize = 5;
-        private const int Min = 10;
+        private const int Min = -5;
         private const int Max = 100;
+        private const ComplexityCase DefaultComplexityCase = ComplexityCase.WorstCase;
         internal readonly int MinDataSetSize = 2;      // made these internal to access from command class
         internal readonly int MaxDataSetSize = 10;
 
@@ -83,7 +83,7 @@ namespace AlgoTeacherWPF.ViewModel
                 IsStable = false,
                 Presentation = new Presentation { BackgroundColor = "Red" }
             };
-            GenerateRandomDataSet();
+            SetupInitialData();
             SetupCommands();
         }
 
@@ -91,7 +91,7 @@ namespace AlgoTeacherWPF.ViewModel
         public AlgorithmDetailViewModel(Algorithm algorithm)
         {
             Algorithm = algorithm;
-            GenerateRandomDataSet();
+            SetupInitialData();
             SetupCommands();
         }
 
@@ -115,6 +115,12 @@ namespace AlgoTeacherWPF.ViewModel
 
         #region helpers
 
+        private void SetupInitialData()
+        {
+            GenerateRandomDataSet();
+            ComplexityCase = DefaultComplexityCase;
+        }
+
         private void SetupCommands()
         {
             DataSetSizeIncreaseCommand = new DataSetSizeIncreaseCommand(this);
@@ -123,32 +129,12 @@ namespace AlgoTeacherWPF.ViewModel
         }
         private void GenerateRandomDataSet()
         {
-            Util.GenerateRandomDataSet(Min, Max, DataSetSize, ref _unsortedDataSet);
+            Util.GenerateRandomDataSet(Min, Max, DataSetSize, ComplexityCase, ref _unsortedDataSet);
         }
 
         private void ReArrangeDataSet()
         {
-            var tempList = UnsortedDataSet.ToList();
-            tempList.Sort();
-            UnsortedDataSet.Clear();
-            switch (ComplexityCase)
-            {
-                case ComplexityCase.BestCase:
-                    // already sorted
-                    break;
-                case ComplexityCase.WorstCase:
-                    tempList.Reverse();
-                    break;
-                case ComplexityCase.AverageCase:
-                default:
-                    // mix first and last
-                    (tempList[0], tempList[^1]) = (tempList[^1], tempList[0]);
-                    break;
-            }
-            foreach (var item in tempList)
-            {
-                UnsortedDataSet.Add(item);
-            }
+            Util.ReArrangeDataSet(ComplexityCase, ref _unsortedDataSet);
         }
         #endregion helpers
     }
