c9a033650978decab4b239f6297d2085aa0d65ca
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-05 08:39:42 +0530

    Algo Details view - analysis section displaying complexity case

---
+++ AlgoTeacherWPF/Converters/ComplexityCaseToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using AlgoTeacherWPF.Model.Enums;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AlgoTeacherWPF.Converters
+{
+    public class ComplexityCaseToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((ComplexityCase)value).GetComplexityStr();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() switch
+            {
+                "Best Case" => ComplexityCase.BestCase,
+                "Average Case" => ComplexityCase.AverageCase,
+                "Worst Case" => ComplexityCase.WorstCase,
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unidentified complexity case")
+            };
+        }
+    }
+}
---
+++ AlgoTeacherWPF/Model/Enums/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoTeacherWPF.Model.Enums
+{
+    public static class EnumExtensions
+    {
+        public static string GetComplexityStr(this ComplexityCase complexityCase)
+        {
+            return complexityCase switch
+            {
+                ComplexityCase.WorstCase => "Worst Case",
+                ComplexityCase.AverageCase => "Average Case",
+                ComplexityCase.BestCase => "Best Case",
+                _ => throw new ArgumentOutOfRangeException(nameof(complexityCase), complexityCase,
+                    "Unidentified complexity case")
+            };
+        }
+    }
+}
