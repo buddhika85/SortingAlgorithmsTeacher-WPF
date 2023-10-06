f1c4f1c51d65d9e10525a65c6f18e7c000517252
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-10-06 14:07:53 +0530

    SortLogMessageTypeToTextColorConverter

---
+++ AlgoTeacherWPF/Converters/SortLogMessageTypeToTextColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using AlgoTeacherWPF.Model.Enums;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AlgoTeacherWPF.Converters
+{
+    public class SortLogMessageTypeToTextColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if (value is SortLogMessageType)
+            return "black";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return SortLogMessageType.NormalMessage;
+        }
+    }
+}
